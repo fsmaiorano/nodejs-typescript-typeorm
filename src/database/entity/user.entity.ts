8f6fb5d2f5b34eb3408e5fa40f9b25bef5bc252a
Fábio <fsmaiorano@gmail.com>
2019-02-15 15:05:50 -0200

    add example of relationship and adjust table name

--- src/database/entity/user.entity.ts
+++ src/database/entity/user.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('users')
 export class User {
     @PrimaryGeneratedColumn()
     public id!: number;
@@ -13,6 +13,10 @@ export class User {
 
     @Column()
     public password: string = '';
+
+    //example
+    //@ManyToOne(type => User, user => user.id, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    //user!: User;
 }
 
 export default User;
